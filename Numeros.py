b4c187ed067551f5b87686caea5d55eb2f7ded1c
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-02-16 18:20:49 -0300

    conceitos numéricos

---
+++ Numeros.py
@@ -0,0 +1,21 @@
+soma = 2 + 2
+print(soma)
+
+subtracao = 50 - 6
+print(subtracao)
+
+divisao = 10 / 2 #divisao sempre retorna um n de ponto flutuante
+print(divisao)
+
+divisao2 = 17 // 3 # descarta a parte fracionaria
+print(divisao2)
+
+resto = 17 % 3 #retorna o resto da divisao
+print(resto)
+
+potencia = 5 ** 2 # 5 ao quadrado
+print(potencia)
+
+largura = 20
+altura = 5 * 9
+print(largura * altura)
