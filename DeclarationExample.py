31aa722a4dc817f785329706d73d0dbe45d02321
Ana Julia <anajuliasilva3304@gmail.com>
2025-03-02 00:39:34 -0300

    aprendendo declarações 'break and continue'

--- DeclarationExample.py
+++ DeclarationExample.py
@@ -26,3 +26,22 @@ list(range(5, 10))
 a = ['Marcos', 'tinha', 'um', 'ponei']
 for i in range(len(a)) :
     print(i, a [i])
+
+
+# break & continue
+# A breakdeclaração sai do envoltório forou whilelaço mais interno:
+for n in range(2, 10) :
+    for x in range(2, n) :
+        if n % x == 0 :
+            print(f"{n} é igual a {x} * {n // x}")
+            break
+
+# A continuedeclaração continua com a próxima iteração do loop:
+for num in range(2, 20) :
+    if num % 2 == 0 :
+        print(f"numero é par {num} ")
+        continue
+    print(f"numero ímpar {num} ")
+
+
+# else in for loop
